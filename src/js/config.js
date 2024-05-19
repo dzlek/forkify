7e4b4e5439a75afd1f164a58b7e5abf5866fd571
dzlek <6896eight@gmail.com>
2024-05-19 14:22:59 +0300

    intl: initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2cd015ef-f13a-410b-aa1e-d60bf3978a33';
+export const MODAL_CLOSE_SEC = 2.5;
